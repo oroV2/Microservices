15bc78ccba7cfff2aeedc97f60af669d6e57a737
oroV2 <onkarpawar58@gmail.com>
2024-05-16 22:38:49 +0530

    Loan microservice docker image creation
    Loan microservice docker image creation

--- loans/src/main/java/com/eazybytes/loans/LoansApplication.java
+++ loans/src/main/java/com/eazybytes/loans/LoansApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -17,11 +18,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
-@ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
-@EnableJpaRepositories("com.eazybytes.loans.repository")
-@EntityScan("com.eazybytes.loans.model")
-@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
-@OpenAPIDefinition(info = @Info(title = "Loans microservice REST API Documentation", description = "EazyBank Loans microservice REST API Documentation", version = "v1", contact = @Contact(name = "Madan Reddy", email = "tutor@eazybytes.com", url = "https://www.eazybytes.com"), license = @License(name = "Apache 2.0", url = "https://www.eazybytes.com")), externalDocs = @ExternalDocumentation(description = "EazyBank Loans microservice REST API Documentation", url = "https://www.eazybytes.com/swagger-ui.html"))
+//@ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
+//@EnableJpaRepositories("com.eazybytes.loans.repository")
+//@EntityScan("com.eazybytes.loans.model")
+//@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+//@OpenAPIDefinition(info = @Info(title = "Loans microservice REST API Documentation", description = "EazyBank Loans microservice REST API Documentation", version = "v1", contact = @Contact(name = "Madan Reddy", email = "tutor@eazybytes.com", url = "https://www.eazybytes.com"), license = @License(name = "Apache 2.0", url = "https://www.eazybytes.com")), externalDocs = @ExternalDocumentation(description = "EazyBank Loans microservice REST API Documentation", url = "https://www.eazybytes.com/swagger-ui.html"))
 public class LoansApplication {
 
 	public static void main(String[] args) {
